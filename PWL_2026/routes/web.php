1193474d41b129db929fe336aa3dd9ad423f7d12
ArdhanFah <101484883+ArdhanFah@users.noreply.github.com>
2026-02-25 07:38:22 +0700

    Praktikum 1

--- PWL_2026/routes/web.php
+++ PWL_2026/routes/web.php
@@ -2,6 +2,45 @@
 
 use Illuminate\Support\Facades\Route;
 
+## Route Dasar
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Selamat Datang";
+});
+
+Route::get('/hello', function () {
+    return "Hello World";
+});
+
+Route::get('/world', function () {
+    return "World";
+});
+
+Route::get('/about', function () {
+    return "Nama: Ardhan Dikri Achmad Fahrudin<br> 
+    NIM: 244107020012";
+});
+
+## Route Parameter
+// Route::get('/user/{nama}', function ($nama) {
+//     return 'Nama Saya ' . $nama;
+// });
+
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-' . $postId . ' Komentar ke-' . $commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Artikel dengan ID ' . $id;
+});
+
+## Route Optional Parameter
+// Route::get('/user/{name?}', function($name = 'null'){
+//     return 'Nama saya ' . $name;
+// });
+
+
+Route::get('/user/{name?}', function($name = 'John'){
+    return 'Nama saya ' . $name;
 });
