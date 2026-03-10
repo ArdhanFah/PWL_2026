6ef13ca5969100878c403439f24b8545d1de5a6e
ArdhanFah <101484883+ArdhanFah@users.noreply.github.com>
2026-03-10 14:15:53 +0700

    Praktikum 2.1

--- Minggu-04/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-04/PWL_POS/app/Http/Controllers/UserController.php
@@ -5,27 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Traits\ForwardsCalls;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        // UserModel::insert($data);
-
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data);
-
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
