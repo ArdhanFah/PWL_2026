7c862a8be7163d92013bb5931e0afc78ff1c2e84
ArdhanFah <101484883+ArdhanFah@users.noreply.github.com>
2026-03-10 14:34:45 +0700

    Parktikum 2.2

--- Minggu-04/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-04/PWL_POS/app/Http/Controllers/UserController.php
@@ -6,14 +6,13 @@ use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Traits\ForwardsCalls;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
