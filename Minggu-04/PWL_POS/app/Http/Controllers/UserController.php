910ee1b477859f7fc8244120e4932ce580612942
ArdhanFah <101484883+ArdhanFah@users.noreply.github.com>
2026-03-11 17:20:21 +0700

    Praktikum 2.4

--- Minggu-04/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu-04/PWL_POS/app/Http/Controllers/UserController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Traits\ForwardsCalls;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
+        $user = UserModel::firstOrNew([
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ]);
+
+        $user->save();
+
         return view('user', ['data' => $user]);
     }
 }
